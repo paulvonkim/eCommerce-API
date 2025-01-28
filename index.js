c2a621c7d7d45beeb1f06f46b00332afc16e415e
darynazin <danayu@ukr.net>
2025-01-28 18:01:20 +0100

    Update index.js and db/index.js with initial setup

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRouter from "./routers/userRouter.js";
+import productRouter from "./routers/productRouter.js";
+import categoryRouter from "./routers/categoryRouter.js";
+import orderRouter from "./routers/orderRouter.js";
+import sequelize from "./db/index.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const NEON = process.env.NEON;
-app.get("/", (req, res) => res.send("Hello, World!"));
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.use(express.json());
+app.get("/", (req, res) => res.send("Hello, World!"));
+app.use("/users", userRouter);
+app.use("/products", productRouter);
+app.use("/categories", categoryRouter);
+app.use("/orders", orderRouter);
+
+
+sequelize.sync();
+app.listen(PORT, () => console.log(`Server is running on port ${port}`));
