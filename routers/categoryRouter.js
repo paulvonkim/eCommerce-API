4cc21302965b36d5835fabaedc1fc00bf8c6cfc6
Paul Kim <ppaauullkkiimm@gmail.com>
2025-01-29 21:38:29 +0100

    error handler and joi

--- routers/categoryRouter.js
+++ routers/categoryRouter.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import CategoryController from "../controllers/categories.js";
+import { validateBody } from "../middleware/errorHandler.js";
+import { categorySchema } from "../schemas/category.schema.js";
 
 const categoryRouter = Router();
 
 categoryRouter.get("/", CategoryController.getCategories);
 categoryRouter.get("/:id", CategoryController.getCategory);
-categoryRouter.post("/", CategoryController.createCategory);
+categoryRouter.post(
+  "/",
+  validateBody(categorySchema),
+  CategoryController.createCategory
+);
 categoryRouter.put("/:id", CategoryController.updateCategory);
 categoryRouter.delete("/:id", CategoryController.deleteCategory);
 
