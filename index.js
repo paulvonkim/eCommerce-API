be80fe69617630f58f093be80b1d09d94d6c161a
EnidaHebib <enidahebib@gmail.com>
2025-01-30 11:18:14 +0100

    fixing product router

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRouter from "./routers/userRouter.js";
-// import productRouter from "./routers/productRouter.js";
+import productRouter from "./routers/productRouter.js";
 import categoryRouter from "./routers/categoryRouter.js";
 import orderRouter from "./routers/orderRouter.js";
 import sequelize from "./db/index.js";
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.get("/", (req, res) => res.send("Hello, World!"));
 app.use("/users", userRouter);
-// app.use("/products", productRouter);
+app.use("/products", productRouter);
 app.use("/api/categories", categoryRouter);
 app.use("/orders", orderRouter);
 
