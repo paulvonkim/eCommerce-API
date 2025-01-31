439aa4e3f51e46fd484c5d652076f025593c0475
darynazin <danayu@ukr.net>
2025-01-31 17:23:26 +0100

    fix/errorHandler fixed

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,7 +2,7 @@ const errorHandler = (err, req, res, next) => {
   process.env.NODE_ENV !== "production" && console.error(err.stack);
 
   res.status(err.statusCode || 500).json({
-    errors: err.errors || [],
+    errors: err.errors && err.errors.length > 0 ? err.errors : [err.message],
   });
 };
 
